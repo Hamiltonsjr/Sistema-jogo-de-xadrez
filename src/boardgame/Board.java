71c597db531145c6c52a53516b1de175b70ae44f
Hamiltonsjr <hamiltonsjr@hotmail.com>
2020-08-12 16:11:43 -0300

    Placing piece on the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -4,7 +4,7 @@ public class Board {
 
     private int rows;
     private int columns;
-    private Piece[][] pieces;
+    private Piece[][] pieces; // matriz de peças do tabuleiro
 
     public Board(int rows, int columns) {
         this.rows = rows;
@@ -35,7 +35,10 @@ public class Board {
     public Piece piece(Position position){
         return pieces[position.getRow()][position.getColumn()];
     }
-
+    public void placePiece(Piece piece, Position position){
+        pieces[position.getRow()][position.getColumn()] = piece;
+        piece.position = position;// e peça protected no mesmo pode ser manipulada.
+    }
 
 
 }
